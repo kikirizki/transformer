3c91c17030ef817f58b953a712e7818b7983f38a
Kiki Rizki Arpiandi <kkrzkrk@gmail.com>
2023-01-08 13:51:18 +0700

    Create sentence to index function

--- dataset.py
+++ dataset.py
@@ -5,7 +5,7 @@ from nltk.tokenize import word_tokenize
 
 
 class AliceInTheWonderlandDataset(Dataset):
-    def __init__(self):
+    def __init__(self, num_words):
         dataset_path = "alice_in_wonderland.txt"
         dataset_download_url = "https://gist.githubusercontent.com/phillipj/4944029/raw/75ba2243dd5ec2875f629bf5d79f6c1e4b5a8b46/"
         if not exists(dataset_path):
@@ -16,10 +16,15 @@ class AliceInTheWonderlandDataset(Dataset):
             self.alice_strings = "".join(f.readlines())
         self.alice_strings = self.preprocess(self.alice_strings)
         self.alice_strings = word_tokenize(self.alice_strings)
-        self.bag_of_words = sorted(list(set(self.alice_strings)))
+        self.vocabularies = sorted(list(set(self.alice_strings)))
+        self.num_words = num_words
 
     def preprocess(self, text):
         text = text.lower()
-        text = text.replace("(","").replace(")","")
+        text = text.replace("(", "").replace(")", "")
         text = ' '.join(text.split())
         return text
+
+    def sentence_to_index(self,text):
+        return [self.vocabularies.index(word) for word in text]
+
