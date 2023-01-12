68340ca389e93b34902c28f7a90f68999ca009d9
Kiki Rizki Arpiandi <kkrzkrk@gmail.com>
2023-01-12 15:13:30 +0700

    Complete train script for `alice in the wonderland` dataset

--- train_alice.py
+++ train_alice.py
@@ -13,6 +13,8 @@ ff_hidden_size = 2
 n_words = 7
 dropout_prob = 0.8
 
+num_epochs = 20
+
 alice_dataset = AliceInTheWonderlandDataset(n_words)
 dataset_loader = DataLoader(alice_dataset, batch_size=batch_Size, drop_last=True)
 n_vocabs = alice_dataset.get_vocabulary_length()
@@ -21,26 +23,31 @@ model = Transformer(d_model, n_vocabs, ff_hidden_size, n_heads, dropout_prob)
 optimizer = torch.optim.Adam(params=model.parameters())
 criterion = nn.CrossEntropyLoss()
 
-
-def train(model, dataset_loader, optimizer, criterion):
+def train(model, dataset_loader, optimizer, criterion, num_epochs):
     model.train()
-    for i, batch in enumerate(dataset_loader):
-        x, y = batch
-        encoder_input = rearrange(x, "batch_size sequence_length -> sequence_length batch_size")
-        y = rearrange(y, "batch_size sequence_length -> sequence_length batch_size")
+    for epoch in range(num_epochs):
+        loss_epoch = 0.0
+        for i, batch in enumerate(dataset_loader):
+            x, y = batch
+            encoder_input = rearrange(x, "batch_size sequence_length -> sequence_length batch_size")
+            y = rearrange(y, "batch_size sequence_length -> sequence_length batch_size")
 
-        decoder_input = y[:-1, :]
+            decoder_input = y[:-1, :]
 
-        output = model(encoder_input, decoder_input)
-        target = torch.nn.functional.one_hot(y % 1, num_classes=n_vocabs)[1:, :].view(-1, n_vocabs).to(float)
+            output = model(encoder_input, decoder_input)
 
-        loss = criterion(output.view(-1, n_vocabs), target)
+            target = torch.nn.functional.one_hot(y % 1, num_classes=n_vocabs)[1:, :].view(-1, n_vocabs).to(float)
 
-        optimizer.zero_grad()
-        loss.backward()
-        torch.nn.utils.clip_grad_norm_(model.parameters(), 0.5)
-        optimizer.step()
+            loss = criterion(output.view(-1, n_vocabs), target)
 
+            optimizer.zero_grad()
+            loss.backward()
+            torch.nn.utils.clip_grad_norm_(model.parameters(), 0.5)
+            optimizer.step()
+            loss_epoch += loss.item()
+        loss_epoch /= len(dataset_loader)
+
+        print(f"Epoch {epoch} loss : {loss_epoch}")
 
 if __name__ == '__main__':
-    train(model, dataset_loader, optimizer, criterion)
+    train(model, dataset_loader, optimizer, criterion, num_epochs)
