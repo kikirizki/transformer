1382d39559a8468398c891f05443cf590c93d780
Kiki Rizki Arpiandi <kkrzkrk@gmail.com>
2023-01-17 12:45:48 +0700

    Save model and fix model forward

--- train_language_model.py
+++ train_language_model.py
@@ -1,3 +1,4 @@
+import os
 import torch
 from einops import rearrange
 from torch import nn
@@ -5,19 +6,24 @@ from torch.utils.data import DataLoader
 
 from dataset import AliceInTheWonderlandDataset
 from vanilla_transformer import VanillaTransformer
+from pathlib import Path
 
 d_model = 512
 n_heads = 2
 batch_Size = 12
 ff_hidden_size = 2
 n_words = 7
-dropout_prob = 0.8
+dropout_prob = 0.1
+save_interval = 5
+num_epochs = 100
+checkpoint_path = "checkpoint"
+Path(checkpoint_path).mkdir(parents=True, exist_ok=True)
 
-num_epochs = 20
 
 alice_dataset = AliceInTheWonderlandDataset(n_words)
 dataset_loader = DataLoader(alice_dataset, batch_size=batch_Size, drop_last=True)
 n_vocabs = alice_dataset.get_vocabulary_length()
+
 
 model = VanillaTransformer(d_model, n_vocabs, ff_hidden_size, n_heads, dropout_prob)
 optimizer = torch.optim.Adam(params=model.parameters())
@@ -35,7 +41,7 @@ def train(model, dataset_loader, optimizer, criterion, num_epochs):
 
             decoder_input = y[:-1, :]
 
-            output = model(encoder_input, decoder_input)
+            output = model(encoder_input, decoder_input, mask)
 
             target = torch.nn.functional.one_hot(y % 1, num_classes=n_vocabs)[1:, :].view(-1, n_vocabs).to(float)
 
@@ -47,7 +53,8 @@ def train(model, dataset_loader, optimizer, criterion, num_epochs):
             optimizer.step()
             loss_epoch += loss.item()
         loss_epoch /= len(dataset_loader)
-
+        if epoch % save_interval == 0:
+          torch.save(model.state_dict(), os.path.join(checkpoint_path, f"checkpoint_{epoch}.pth"))
         print(f"Epoch {epoch} loss : {loss_epoch}")
 
 if __name__ == '__main__':
