22fec23bd3949f7a28a87705ee4bc7c511b87409
Kiki Rizki Arpiandi <kkrzkrk@gmail.com>
2023-01-25 14:28:34 +0700

    Remove softmax from vanilla transformer

--- vanilla_transformer.py
+++ vanilla_transformer.py
@@ -10,11 +10,10 @@ class VanillaTransformer(nn.Module):
         self.encoder = TransformersEncoder(d_model, ff_hidden_size, n_heads, dropout_prob)
         self.decoder = TransformersDecoder(d_model, ff_hidden_size, n_heads, dropout_prob)
         self.linear = torch.nn.Linear(d_model, n_vocab)
-        self.softargmax = torch.nn.Softmax(dim=2)
+
 
     def forward(self, encoder_input, decoder_input, target_mask=None, source_mask=None):
         encoder_output = self.encoder(self.embedding(encoder_input), source_mask)
         decoder_output = self.decoder(self.embedding(decoder_input), src=encoder_output, mask=target_mask)
         output = self.linear(decoder_output)
-        probability = self.softargmax(output)
-        return probability
+        return output
